834685176f531f4027b46e3187275ea6182f25dd
brandon <brandon23jimenez3@gmail.com>
2023-12-28 18:36:37 +0100

    Created user Dao

---
+++ src/main/java/cat/copernic/project2/ERP/dao/UserDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package cat.copernic.project2.ERP.dao;
+
+import cat.copernic.project2.ERP.domain.User;
+
+/**
+ *
+ * @author brandon
+ */
+public interface UserDao extends GenericDao<User, Long> {
+        
+}
