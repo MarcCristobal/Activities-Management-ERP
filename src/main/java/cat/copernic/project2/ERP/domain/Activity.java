7c25296f2dff68ab4ab3760f2bcdc7e4a1f89572
Oscar <melee_oscarbp@hotmail.com>
2024-01-09 14:10:00 +0100

    Activities Service done

---
+++ src/main/java/cat/copernic/project2/ERP/domain/Activity.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cat.copernic.project2.ERP.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.util.Date;
+import lombok.Data;
+
+/**
+ *
+ * @author oscar
+ */
+@Data
+@MappedSuperclass
+public class Activity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(length = 50)
+    private String place;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private boolean isFree;
+
+    private int participantLimit;
+
+    private double pricePerPerson;
+
+    private int numberOfPayments;
+
+    public boolean isIsFree() {
+        return isFree;
+    }
+
+    public void setIsFree(boolean isFree) {
+        this.isFree = isFree;
+    }
+
+}
