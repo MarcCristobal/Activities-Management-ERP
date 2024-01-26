11f67822da43fc7f83cdf9f03a42ae83f75108e4
Marc <marc7cs@gmail.com>
2024-01-26 13:41:39 +0100

    - resources and requirements lists dinamic bg function added and fixed mistakes

--- src/main/resources/static/js/activityForm.js
+++ src/main/resources/static/js/activityForm.js
@@ -27,8 +27,15 @@ document.addEventListener('DOMContentLoaded', function () {
         toggleParticipantsSection();
     });
 
+    // Adjunta event listeners para la lista de recursos y la lista de requisitos
+    attachDeleteListeners('resourceList');
+    attachDeleteListeners('requirementList');
+
     toggleParticipantsSection();
     togglePaymentOptions();
+
+    applyBackground('resourceList');
+    applyBackground('requirementList');
 
     function addListItem(listId, inputId) {
         var input = document.getElementById(inputId);
@@ -39,7 +46,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Crea un nuevo elemento de lista y asigna el texto
         var newListItem = document.createElement('li');
-        newListItem.textContent = text;
+        newListItem.className = "flex items-center justify-between group"; // Aplica las clases necesarias al <li>
+
+        // Crea un span para el texto del elemento de lista
+        var spanText = document.createElement('span');
+        spanText.textContent = text;
+
+        // Crea una imagen para el ícono de eliminar
+        var trashIcon = document.createElement('img');
+        trashIcon.src = "/images/contenedor-de-basura2.png";
+        trashIcon.className = "size-4 invisible transition duration-300 ease-in-out group-hover:visible"; // Aplica las clases necesarias al ícono de eliminar
+
+        // Adjunta un event listener a la imagen de basura recién creada
+        trashIcon.addEventListener('click', function () {
+            deleteListItem(trashIcon); // Llama a la función deleteListItem cuando se hace clic en la imagen
+        });
+
+        // Agrega el texto y el ícono de eliminar al nuevo elemento de lista
+        newListItem.appendChild(spanText);
+        newListItem.appendChild(trashIcon);
 
         // Obtén la cantidad de elementos en la lista antes de agregar el nuevo elemento
         var index = list.children.length;
@@ -54,6 +79,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Borra el contenido del campo de entrada
         input.value = '';
+
+        // Luego de agregar el elemento, aplicar el fondo correspondiente
+        applyBackground(listId);
     }
 
     function updateHiddenField(listId) {
@@ -92,6 +120,43 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             pricePerPersonContainer.classList.add('invisible');
             numberOfPaymentsContainer.classList.add('invisible');
+        }
+    }
+
+    // Función para agregar event listeners a las imágenes dentro de una lista dada
+    function attachDeleteListeners(listId) {
+        // Obtén todas las imágenes dentro de la lista con el ID dado
+        var listImages = document.querySelectorAll('#' + listId + ' img');
+
+        // Itera sobre cada imagen y asigna un event listener
+        listImages.forEach(function (image) {
+            image.addEventListener('click', function () {
+                deleteListItem(this); // Llama a la función deleteListItem cuando se hace clic en la imagen
+            });
+        });
+    }
+
+    function deleteListItem(image) {
+        var listItem = image.parentNode; // Obtén el elemento <li> que contiene la imagen
+        var listId = listItem.parentNode.id; // Obtén el ID de la lista a partir del elemento <ul>
+
+        // Elimina el elemento <li> de la lista en el DOM
+        listItem.parentNode.removeChild(listItem);
+
+        // Después de eliminar el elemento, aplicar el fondo correspondiente
+        applyBackground(listId);
+    }
+
+    function applyBackground(listId) {
+        var list = document.getElementById(listId);
+        var listItems = list.children;
+
+        for (var i = 0; i < listItems.length; i++) {
+            if (i % 2 !== 0) {
+                listItems[i].classList.add('bg-gray-200');
+            } else {
+                listItems[i].classList.remove('bg-gray-200');
+            }
         }
     }
 
